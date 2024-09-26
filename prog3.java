82e2a0c904ed6addf2c2eb18a04b7d8aca0868a9
manoj choudhary <132427243+manojchoudhary404@users.noreply.github.com>
2024-09-26 23:26:58 +0530

    Add files via upload

---
+++ prog3.java
@@ -0,0 +1,12 @@
+public class prog3 {
+    public static void main(String args[]){
+        int rows = 4;
+        int cols = 5;
+        for(int i = 1; i<=rows ; i++){
+            for(int j= 1;j<=cols;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
